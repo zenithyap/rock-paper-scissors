a9794f0e2b7ed90eacf0da4d7c180da1cc3169c8
zenith <zenithyap88@gmail.com>
2024-12-20 14:45:23 +0800

    Add logic for playing a single round

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getChoice(choice) {
     if (choice === ROCK) {
         return 'rock';
@@ -23,3 +26,27 @@ function getHumanChoice() {
 
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    console.log(`You chose ${humanChoice}`);
+    console.log(`Computer chose ${computerChoice}`);
+
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') 
+        || (humanChoice === 'scissors' && computerChoice === 'paper') 
+        || (humanChoice === 'paper' && computerChoice === 'rock')) {
+            console.log(`You win! ${humanChoice} wins ${computerChoice}`);
+            humanScore += 1;
+    } else if (humanChoice === computerChoice) {
+        console.log('Its a draw!');
+    } else {
+        console.log(`You lose! ${computerChoice} wins ${humanChoice}`);
+        computerScore += 1;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
